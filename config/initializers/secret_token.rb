0bcda013e6e49fea8ffac130aa1c812f29d60e43
stevo <blazejek@gmail.com>
2014-02-18 13:35:36 +0100

    application scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ototeam::Application.config.secret_key_base = '83f775b2a513b61816431f07442a36fc49a272c033a579e61754f679e768f94c4e9993cfd5c7ac4c5c8932d90b63518657b7ce0704a4caf2670f88a64a434730'
