c4f96950d2bfb3bcca0bce155e18991208b28a33
stevo <blazejek@gmail.com>
2014-02-18 14:28:01 +0100

    generate only necessary specs

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module Ototeam
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+    end
   end
 end
