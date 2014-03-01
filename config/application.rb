dd8004b689599c84b8ed4ddede15183a300eeede
tbprojects <info.tbprojects@gmail.com>
2014-03-01 20:49:32 +0100

    defined fixture replacement in application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module Ototeam
                        helper_specs: false,
                        routing_specs: false,
                        request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
 end
