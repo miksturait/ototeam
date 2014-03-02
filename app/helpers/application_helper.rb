a4138ffe8e4f639a3d32b819d718271e82f1ded8
tbprojects <info.tbprojects@gmail.com>
2014-03-02 15:20:56 +0100

    icon_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def icon_link_to(icon, name, path, options = {})
+    link_to path, options do
+      content_tag(:span, nil, class: "glyphicon glyphicon-#{icon}") + " #{name}"
+    end
+  end
 end
