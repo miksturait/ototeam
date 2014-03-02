1002eee59bd2bb9343ba16118086483ef2dd6151
tbprojects <info.tbprojects@gmail.com>
2014-03-02 16:56:51 +0100

    validations for friends

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,9 +1,19 @@
 class Friend < ActiveRecord::Base
+  validates :fullname, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, allow_blank: true
+  validate :has_email_or_phone
+
   def group_names
     'Piłka, Bar'
   end
 
   def attendance_percentage
     '50%'
+  end
+
+  private
+
+  def has_email_or_phone
+    errors.add(:base, 'Please define email or phone') if email.blank? and phone.blank?
   end
 end
