4f7ac9a3ce17f9712bbf89b88662c0f6443554e6
tbprojects <info.tbprojects@gmail.com>
2014-03-02 17:14:22 +0100

    validations for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,24 @@
 class Event < ActiveRecord::Base
+  before_validation :fill_invite_from, unless: :invite_from
+  before_validation :fill_invite_to, unless: :invite_to
+  validates :name, :start_at, :minutes_for_answer, presence: true
+  validate :timeline_is_correct, if: :start_at
+
   def attendees_count
     0
+  end
+
+  private
+
+  def fill_invite_from
+    self.invite_from = DateTime.now
+  end
+
+  def fill_invite_to
+    self.invite_to = start_at
+  end
+
+  def timeline_is_correct
+    errors.add(:base, 'Dates order is invalid') if invite_from >= invite_to or invite_to > start_at
   end
 end
