17196498b3572823bb54d31419e6826d5482298e
krypton9208 <krypton9208@gmail.com>
2014-03-04 17:33:11 +0100

    my commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# ruby encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,14 +6,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-class AddInitialGroup < ActiveRecord::Migration
   def up
-    5.times do |i|
-     Group.in("grupa ##{i}")
-    end
+
+     Group.new(name: "grupa ##{i}")
   end
 
   def down
    Group.destroy_all
   end
-end
