da408cce3dd343e736e29e36d72981b622c533ce
krypton9208 <krypton9208@gmail.com>
2014-03-05 23:18:46 +0100

    dodano rejestracje i logowanie oraz autoryzacje + poprawki

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Ototeam::Application.routes.draw do
 
   resources :posts
 
-  root 'posts#index'
+  root 'events#index'
   resources :slides, only: %i(index show)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
