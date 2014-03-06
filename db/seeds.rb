744c57d5e48e67c5f39820685d390b3ab896a59b
tbprojects <info.tbprojects@gmail.com>
2014-03-06 11:37:00 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
-FactoryGirl.create_list(:event, 10)
-FactoryGirl.create_list(:friend, 10)
-FactoryGirl.create_list(:group, 10)
+ActiveRecord::Base.transaction do
+  user = FactoryGirl.create(:user)
+  FactoryGirl.create_list(:event, 10, creator: user)
+  FactoryGirl.create_list(:friend, 10, creator: user)
+  FactoryGirl.create_list(:group, 10, creator: user)
+  puts "User: #{user.email} Password: secret"
+end
