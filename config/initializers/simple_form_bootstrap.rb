fb6cc0fcf07688273a748b795b6e23e91d2b6a6f
tbprojects <info.tbprojects@gmail.com>
2014-03-06 09:48:17 +0100

    search form wrapper for simple_form

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -45,5 +45,22 @@ SimpleForm.setup do |config|
     b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
   end
 
+  config.wrappers :search_form, tag: 'div', class: 'form-group', error_class: 'has-error',
+                  defaults: { input_html: { class: 'form-group default_class' } } do |b|
+
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.wrapper tag: 'div' do |input_block|
+      input_block.use :input
+      input_block.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    end
+  end
+
   config.default_wrapper = :bootstrap3_horizontal
 end
