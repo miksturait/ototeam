57563a1447580bf55714d65a5d83df195f78e54f
tbprojects <info.tbprojects@gmail.com>
2014-03-09 16:52:27 +0100

    attribute_entry helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
       content_tag(:span, nil, class: "glyphicon glyphicon-#{icon}") + " #{name}"
     end
   end
+
+  def attribute_entry(object, field)
+    content_tag(:dt, "#{object.class.human_attribute_name(field)}:") +
+    content_tag(:dd, object.send(field))
+  end
 end
