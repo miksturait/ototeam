d5ec9e84870a12ff43b30ee074a239aea7142e22
tbprojects <info.tbprojects@gmail.com>
2014-03-09 16:17:20 +0100

    user has his own profile (friend)

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,11 +1,11 @@
 class Friend < ActiveRecord::Base
+  belongs_to :user
   belongs_to :creator, class_name: User
   has_many :group_memberships
   has_many :groups, through: :group_memberships
 
-  validates :fullname, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, allow_blank: true
-  validate :has_email_or_phone
+  validate :has_email_or_phone, unless: :user
 
   def group_names
     groups.map(&:name).to_sentence
@@ -13,6 +13,10 @@ class Friend < ActiveRecord::Base
 
   def attendance_percentage
     '50%'
+  end
+
+  def current_email
+    user.try(:email) || email
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ActiveRecord::Base
 
   has_many :events, foreign_key: :creator_id, dependent: :destroy
   has_many :groups, foreign_key: :creator_id, dependent: :destroy
-  has_many :friends, foreign_key: :creator_id, dependent: :destroy
+  has_many :friends, ->(obj){ where.not(user_id: obj.id) }, foreign_key: :creator_id, dependent: :destroy
+
+  has_many :profiles, foreign_key: :user_id, class_name: Friend, dependent: :nullify
+  has_one :own_profile, ->(obj){ where(creator_id: obj.id) }, foreign_key: :user_id, class_name: Friend, dependent: :destroy
+  accepts_nested_attributes_for :own_profile
+
+  after_create :create_own_profile
 end
